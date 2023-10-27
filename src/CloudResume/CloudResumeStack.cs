09a796b47330cd9b9e0d712425f47fbde8e8dce6
Andre Burke <aburke.dev@gmail.com>
2023-10-26 22:40:28 -0600

    Initial commit

---
+++ src/CloudResume/CloudResumeStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace CloudResume
+{
+    public class CloudResumeStack : Stack
+    {
+        internal CloudResumeStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
